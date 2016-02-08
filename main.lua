7c483bbd9c973e1009d8d21d89a58842c36e5e66
Adrienne Walker <enne@quisquo.us>
2016-02-07 22:17:05 -0800

    Add player group of cooldown tracking by job

--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
-﻿PlayerMap = {}
-Icons = {}
+﻿PlayerTrackers = {}
 
-GlobalIcon = {}
+-- debugging
 GlobalText = {}
 
-TempLastTick = 0
-TempCooldown = 60
+Debug = function(text)
+	if GlobalText.Text == nil then
+		return
+	end
+	GlobalText.Text = GlobalText.Text .. "\n" .. text
+end
 
 UI.OnLoad = function()
 	Initialize()
@@ -13,18 +16,14 @@ end
 
 UI.OnFrame = function(ticks)
 	ProcessEvents(ticks)
-	
-	local t = math.ceil((TempLastTick - ticks) / 1000 + TempCooldown)
-	GlobalIcon:SetTime(t)
 end
 
 CooldownIcon = {}
-function CooldownIcon:new(o, filepath, x, y, w, h)
-	o = o or {}
-	setmetatable(o, self)
-	self.__index = self
-	self.ShowShame = true
+CooldownIcon.__index = CooldownIcon
+function CooldownIcon:new(filepath, x, y, w, h, showShame)
+	local self = setmetatable({}, CooldownIcon)
 	
+	self.ShowShame = showShame
 	self._Showing = false
 	
 	local fontSize = 32
@@ -85,7 +84,7 @@ function CooldownIcon:new(o, filepath, x, y, w, h)
 	self._MainText.FontWeight = fontWeight
 	
 	self.Time = -1
-	self:SetTime(56)
+	self:SetTime(0)
 	
 	return self
 end
@@ -160,10 +159,98 @@ function CooldownIcon:Hide()
 	self._MainText:Hide()
 end
 
-function Initialize()
-	GlobalIcon = CooldownIcon:new(nil, "Icons/000000/000461.png", 100, 100, 40, 40)
-	GlobalIcon:Show()
+PlayerTracker = {}
+PlayerTracker.__index = PlayerTracker
+function PlayerTracker:new(playerID, x, y, w, h)
+	local self = setmetatable({}, PlayerTracker)
+	self._Showing = false
+	self._X = x
+	self._Y = y
+	self._W = w
+	self._H = h
+	self._Margin = 2
+	self._LastUseBySkill = {}
+	self._SkillMap = {}
+	self._IconList = {}
+	self._Job = nil
+	return self
+end
+
+function PlayerTracker:UseSkill(skillID, ticks)
+	self._LastUseBySkill[skillID] = ticks
+	Debug("Use skill: " .. skillID)
+end
+
+function PlayerTracker:Update(ticks)
+	for skillID, icon in pairs(self._IconList) do
+		local lastUse = self._LastUseBySkill[skillID]
+		if lastUse ~= nil then
+			local cooldownSeconds = self._SkillMap[skillID].Recast
+			local seconds = math.ceil((lastUse - ticks) / 1000 + cooldownSeconds)
+			icon:SetTime(seconds)
+		end
+	end
+end
+
+function PlayerTracker:SetJob(jobName)
+	if self._Job == jobName then
+		return
+	end
+
+	local wasShowing = self._Showing
+	self:Hide()
+
+	self._Job = jobName
+	self._IconList = {}
+	self._SkillMap = {}
+	-- Changing jobs is really confusing.  Some skills stay on cooldown (benediction),
+	-- but others with the same skill ID may reset if in a sanctuary (swiftcast).
+	-- Give up and assume everything is reset.  /o\
+	self._LastUseBySkill = {}
 	
+	-- TODO: Make a per-job config file, i.e. whm = { "Swiftcast", "Benediction" }
+	-- for both ordering and which skills are wanted to be displayed rather than
+	-- just using everything in the constant list.
+	local skillList = _Cooldowns[jobName]
+	if skillList == nil then
+		return
+	end
+	local x = self._X
+	local y = self._Y
+	for i = 1, #skillList do
+		local skill = skillList[i]
+		self._IconList[skill.ID] = self:_MakeIcon(skill, x, y)
+		self._SkillMap[skill.ID] = skill
+		Debug(tostring(i) .. ", " .. skill.Name .. ", " .. skill.ID .. ", " .. x .. ", " .. tostring(self._IconList[skill.ID]))
+		x = x + self._W + self._Margin
+	end
+
+	if wasShowing then
+		self:Show()
+	end
+end
+
+function PlayerTracker:_MakeIcon(skill, x, y)
+	local iconFolder = "Icons/"
+	local icon = CooldownIcon:new(iconFolder .. skill.Icon, x, y, self._W, self._H, skill.Shame)
+	return icon
+end
+
+function PlayerTracker:Show()
+	self._Showing = true
+	for _, icon in pairs(self._IconList) do
+		icon:Show()
+	end
+end
+
+function PlayerTracker:Hide()
+	self._Showing = false
+	for _, icon in pairs(self._IconList) do
+		icon:Hide()
+	end
+end
+
+function Initialize()
 	GlobalText = UI.NewLabel('High')
 	GlobalText.FontSize = 12
 	GlobalText.X = 20
@@ -173,21 +260,31 @@ function Initialize()
 	GlobalText.Color = "#FFFFFFFF"
 	GlobalText.FontFamily = "Sansation"
 	GlobalText.FontWeight = 200
-	GlobalText.Text = "foobar"
 	GlobalText:Show()
 end
 
 function ProcessEvents(ticks)
 	local player = FF.GetPlayer()
+
+	-- Hack player in for now before finding party list
+	if player and PlayerTrackers[player.ID] == nil then
+		PlayerTrackers[player.ID] = PlayerTracker:new(player.ID, 100, 200, 40, 40)
+		PlayerTrackers[player.ID]:Show()
+	end
+	if player then
+		PlayerTrackers[player.ID]:SetJob(player.Job)
+	end
+
 	local combatEvents = FF.GetAllCombatEvents()
-	local i
 	for i = 0, combatEvents.Length - 1 do
 		local event = combatEvents[i]
-		if event.ActorID == player.ID and event.MessageType == "SingleAbility" then
-			if event.SkillID == 150 then
-				TempLastTick = ticks
+		if event.MessageType == "SingleAbility" then
+			if PlayerTrackers[event.ActorID] then
+				PlayerTrackers[event.ActorID]:UseSkill(event.SkillID, ticks)
 			end
-			GlobalText.Text = tostring(event.SkillID)
 		end
+	end
+	for _, tracker in pairs(PlayerTrackers) do
+		tracker:Update(ticks)
 	end
 end
